beb7a49cc85f084365aab40e65c319f91803110a
iRLprogrammerman <103297888+iRLprogrammerman@users.noreply.github.com>
2022-04-29 23:47:55 -0600

    Update test_atm.py

--- test_atm.py
+++ test_atm.py
@@ -5,7 +5,7 @@ from users import *
 util = login_utility()
 
 
-class LoginTests(unittest.TestCase):
+class test_case1(unittest.TestCase):
 
     def testLogInSuccess(self):
         self.assertTrue(login(*util, "Ryan", "ryanpassword"), "login failure")
@@ -14,7 +14,7 @@ class LoginTests(unittest.TestCase):
         self.assertFalse(login(*util, "Ryan", None), "entered password grants access")
 
 
-class SignupTests(unittest.TestCase):
+class test_case2(unittest.TestCase):
 
     def testSignUpSuccess(self):
         self.assertTrue(signup(*util, "test", "test", "test"), "signup failure")
@@ -26,22 +26,13 @@ class SignupTests(unittest.TestCase):
         self.assertFalse(signup(*util, "test2", "test", "bro"))
 
 
-class DepositTests(unittest.TestCase):
+class test_case3(unittest.TestCase):
 
     def testDeposit50(self):
         self.assertEqual(50, deposit(*util, "Ryan", 50), "You did not deposit $50")
 
     def testResponseType(self):
         self.assertRaises(TypeError, deposit(*util, "Ryan", "ok and?"))
-
-
-class WithdrawalTests(unittest.TestCase):
-
-    def testWithdraw50(self):
-        self.assertEqual(50, withdraw(*util, "Ryan", 50), "You did not withdraw $50")
-
-    def testResponseType(self):
-        self.assertRaises(TypeError, withdraw(*util, "Ryan", "str"))
 
 
 # "username" is a primary key, tests are broken if "test" and "test2" are not removed
